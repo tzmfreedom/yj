a54dc38cf80c9954c64a82d7d8fcbd324b4de919
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-11-12 12:29:29 +0900

    Use MultiJson to match to method with jbuilder json serialization

--- lib/yj/handler.rb
+++ lib/yj/handler.rb
@@ -1,5 +1,6 @@
 require 'action_dispatch/http/mime_type'
 require 'yj/render'
+require 'multi_json'
 
 module Yj
   class Handler
@@ -7,10 +8,15 @@ module Yj
     self.default_format = Mime[:json]
 
     def self.call(template)
-      %{_partial ||= false; r ||= Yj::Render.new(self); yaml = ERB.new("#{template.source}", nil, '-').result(binding);
-      return JSON.dump(YAML.load(yaml)) unless _partial; yaml.each_line.with_index.map do |line, idx|
+      <<~SRC
+      _partial ||= false
+      r ||= Yj::Render.new(self)
+      yaml = ERB.new("#{template.source}", nil, '-').result(binding)
+      return MultiJson.dump(YAML.load(yaml)) unless _partial
+      yaml.each_line.with_index.map do |line, idx|
         idx == 0 ? "- \#{line}" : "  \#{line}"
-      end.join}
+      end.join
+      SRC
     end
   end
 end
